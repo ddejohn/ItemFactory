716fcf488596566072b03edbb9633e24f486ad2f
Devon DeJohn <ddejohn@pm.me>
2021-08-02 00:40:30 -0600

    adds drill-down for any args

--- factory/item.py
+++ factory/item.py
@@ -1,44 +1,36 @@
-# Standard Library
-from typing import Any
-from random import choice
-
-# Third party
-import yaml
-
-# Local
-import data.string_templates as templates
-
-
 class ItemBase:
     """Base class for Item"""
-    def __init__(self, *, item_type=None,
-                          item_class=None,
-                          item_subclass=None,
-                          item_make=None):
-        with open("ItemFactory/data/items.yml") as item_data:
-            item_types = yaml.safe_load(item_data.read())
+    def __init__(self, *, item_class, item_subclass, item_type, item_subtype):
+        self.item_class = item_class
+        self.item_subclass = item_subclass
+        self.item_type = item_type
+        self.item_subtype = item_subtype
 
-        # Needs two-way dictionaries, lookup needs to start at 'make'
-        # Likely need enumeration
-        # self.item_type = self.get_data(item_types, item_type)
-
-        # item_classes = item_types[item_type]
-        # self.item_class = self.get_data(item_classes, item_class)
-
-        # item_subclasses = item_classes[self.item_class]
-        # self.item_subclass = self.get_data(item_subclasses, item_subclass)
-
-        # item_makes = item_subclasses[self.item_subclass]
-        # self.item_make = self.get_data(item_makes, item_make)
-
-    def get_data(d: dict, key: Any) -> str:
-        return {None: choice(d.keys())}.get(key, key)
+    def embed(self, primary_field: dict, secondary_field: dict) -> dict:
+        class_subclass = f"{self.item_class} ({self.item_subclass})"
+        return {"title": self.name,
+                "description": f"*{self.description}*",
+                "color": 0x40444b,
+                "fields": [{"name": "Class (subclass)",
+                            "value": class_subclass,
+                            "inline": True},
+                           {"name": "Type",
+                            "value": self.item_type,
+                            "inline": True},
+                           {"name": "Subtype",
+                            "value": self.item_subtype,
+                            "inline": True},
+                           {"name": "Rarity",
+                            "value": self.rarity,
+                            "inline": True},
+                           primary_field,
+                           secondary_field]}
 
 
 class ItemAttributes(ItemBase):
     """Attributes for Item objects"""
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, **item_data: dict):
+        super().__init__(**item_data)
         self.rarity = ""
         self.primary = ""
         self.secondary = ""
@@ -46,29 +38,51 @@ class ItemAttributes(ItemBase):
 
 
 class Item(ItemAttributes):
-    """The top-level Item object"""
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    """Common class for Armor and Weapon"""
+    def __init__(self, **item_data: dict):
+        super().__init__(**item_data)
         self.name = ""
         self.description = ""
-        self.stats = {}
 
-    def __str__(self):
-        if self.item_type == "armor":
-            stats = templates.ARMOR_STATS
-            details = templates.ARMOR_DETAILS
-        else:
-            stats = templates.WEAPON_STATS
-            details = templates.WEAPON_DETAILS
 
-        stats = stats.format(**self.stats)
-        details = details.format(primary=self.primary,
-                                 secondary=self.secondary)
+class Armor(Item):
+    """Top-level class for armor items"""
+    def __init__(self, **item_data: dict):
+        super().__init__(**item_data)
 
-        return templates.ITEM_INFO.format(name=self.name,
-                                          description=self.description,
-                                          item_type=self.item_type,
-                                          item_class=self.item_class,
-                                          item_subclass=self.item_subclass,
-                                          details=details,
-                                          stats=stats)
+    @property
+    def embed(self):
+        primary_field = {"name": "Material",
+                         "value": self.primary,
+                         "inline": True}
+        secondary_field = {"name": "Construction",
+                           "value": self.secondary,
+                           "inline": True}
+        return super().embed(primary_field, secondary_field)
+
+
+class Weapon(Item):
+    """Top-level class for weapon items"""
+    def __init__(self, **item_data: dict):
+        super().__init__(**item_data)
+
+    @property
+    def embed(self):
+        primary_field = {"name": "Primary Material",
+                         "value": self.primary,
+                         "inline": True}
+        secondary_field = {"name": "Secondary Material",
+                           "value": self.secondary,
+                           "inline": True}
+        return super().embed(primary_field, secondary_field)
+
+
+if __name__ == "__main__":
+    test = Weapon(**{"item_class": "weapon",
+                     "item_subclass": "two-handed",
+                     "item_type": "blade",
+                     "item_subtype": "bastard sword"})
+    test.rarity = "Legendary"
+    test.primary = "Meteorite"
+    test.secondary = "Ebony"
+    print(test.embed)
